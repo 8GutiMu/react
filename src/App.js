9099d8b9472f4857af1c18852d250794c35c22d6
8GutiMu <you@example.com>
2017-07-28 16:16:57 -0500

    agregando

--- src/App.js
+++ src/App.js
@@ -8,42 +8,33 @@ import { Row,Col,Grid } from 'react-bootstrap';
 
 
 class App extends Component {
-  render() {
-	  
+	
 	constructor(){
+		super();
 		this.state = {
-			elementosDeListaCarros = []
+			"elementosDeListaCarros":[],
+			"textoDeInput":[]
 		}
 	}
+	
+	eventoPrueba(){
+		alert("Agregamos un caracter al input")
+	}
+	
+	render() {
+
     return (
      <div className="App">
 
-		<Menu itemsMenu={["HOME","ABOUT US","CONTACT"]}/>
-		<h1>Hola Mundo</h1>
 		
-	 	{/* Para usar el grid */}
-
-		<Grid>
-			<Row className="show-grid">
-			  <Col  md={8}>Col4</Col>
-			  <Col  md={4}>col4</Col>
-			</Row>
-		</Grid>
-		
+		<input type="text" onChange={this.eventoPrueba}/>
+		<button>Agregar</button>
 		
 		
 		{/* Listas */}
 		<Lista titulo="Carros" elementosDeLista={["ford","chevy"]}/>
 		
-		<Lista titulo="Super HEroes" elementosDeLista={["batman","superman","hola"]}/>
-			
-	  	<Lista titulo="Colores" elementosDeLista={["azul","rosa","morado","amarillo"]}/>
 		
-			
-		
-		<Footer colorTexto="rgb(0,250,3)" textoFooter= "1 Adios Mundo" />
-		<Footer colorTexto="#e3e3e3" textoFooter= "2 Adios Mundo"/>
-		<Footer colorTexto="red" textoFooter= "3 Adios Mundo"/>
 	
 	</div>
     );
